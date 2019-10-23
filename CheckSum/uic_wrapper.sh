8838d1d255ebca248b4861f9aff5c32e07c95dc6
Xiao Jie <xiaojie@basecity.com>
2019-10-23 21:14:00 +0800

    Update project settings

--- CheckSum/uic_wrapper.sh
+++ CheckSum/uic_wrapper.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-DYLD_LIBRARY_PATH=/Users/jie/My/Study/Qt/qt-everywhere-src-5.11.0/qtbase/lib${DYLD_LIBRARY_PATH:+:$DYLD_LIBRARY_PATH}
+DYLD_LIBRARY_PATH=/Users/jie/my/study/qt/qt5/qtbase/lib${DYLD_LIBRARY_PATH:+:$DYLD_LIBRARY_PATH}
 export DYLD_LIBRARY_PATH
-QT_PLUGIN_PATH=/Users/jie/My/Study/Qt/qt-everywhere-src-5.11.0/qtbase/plugins${QT_PLUGIN_PATH:+:$QT_PLUGIN_PATH}
+QT_PLUGIN_PATH=/Users/jie/My/Study/Qt/qt5/qtbase/plugins${QT_PLUGIN_PATH:+:$QT_PLUGIN_PATH}
 export QT_PLUGIN_PATH
-exec /Users/jie/My/Study/Qt/qt-everywhere-src-5.11.0/qtbase/bin/uic "$@"
+exec /Users/jie/my/study/qt/qt5/qtbase/bin/uic "$@"
